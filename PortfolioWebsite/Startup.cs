559b765c32277d643755585ec4e2fd3495c90b3e
Nicolas Sirimongkol <nsirimo@calstatela.edu>
2017-12-31 14:41:41 -0800

    Created a fresh new mvc web project from scratch!

--- PortfolioWebsite/Startup.cs
+++ PortfolioWebsite/Startup.cs
@@ -20,15 +20,8 @@ namespace PortfolioWebsite
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
