1b73e4299c3a97ad2349c2497eec663bb5a518f8
Nicolas Sirimongkol <nsirimo@calstatela.edu>
2018-01-02 17:04:09 -0800

    Added Service to Startup for MVC. Added cfg Maproute.

--- PortfolioWebsite/Startup.cs
+++ PortfolioWebsite/Startup.cs
@@ -15,16 +15,18 @@ namespace PortfolioWebsite
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDefaultFiles();
             app.UseStaticFiles();
 
             app.UseMvc(cfg => {
-                cfg.MapRoute("Foo", "/users/manage", new { controller = "UserManagement", Action = "index" });
+                cfg.MapRoute("Default",
+                    "{controller}/{action}/{id?}",
+                    new { controller = "App", Action = "index" });
                 });
         }
     }
